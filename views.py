b4631ed8b983a41f97680d8e26d02f9089feeb1b
AlexsanderTG <80927240+AlexsanderTG@users.noreply.github.com>
2024-06-13 18:31:17 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,113 @@
+import os
+from helpers import recupera_imagem, deleta_arquivo
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
+from jogoteca import app, db
+from models import Jogos, Usuarios
+from config import UPLOAD_PATH
+import time
+app.config['UPLOAD_PATH'] = UPLOAD_PATH
+# Rotas do aplicativo Flask
+@app.route('/')
+def index():
+    lista= Jogos.query.order_by(Jogos.id)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html', titulo="Jogos", jogos=lista)
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo="Novo Jogo")
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo= Jogos.query.filter_by(nome=nome).first()
+    if jogo:
+        flash('jogo ja existe')
+        return redirect(url_for('index'))
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}-{timestamp}.jpg')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = Jogos.query.filter_by(id=id).first()
+    capa_jogo = recupera_imagem(id)
+    return render_template('editar.html', titulo="Editar Jogo", jogo=jogo, capa_jogo=capa_jogo)
+
+@app.route('/atualizar', methods=['POST'])
+def atualizar():
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+
+    db.session.add(jogo)
+    db.session.commit()
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    deleta_arquivo(jogo.id)
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login'))
+
+    jogo = Jogos.query.filter_by(id=id).delete()
+    db.session.commit()
+    flash('Jogo deletado com sucesso')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+   usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+   if usuario:
+        if request.form['senha'] == usuario.Senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+        else:
+            flash('Usuário não logado, erro de autenticação!')
+            return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
+
+def recupera_imagem(id):
+    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
+        if f'capa{id}.jpg'==nome_arquivo:
+            return nome_arquivo
+    return 'capa_padrão.jpg'
